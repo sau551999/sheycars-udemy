65e85edcd36262e7f7c08ce2df4c2114ee8eadd1
saurabhprabhu <saurabhprabhu14@gmail.com>
2024-03-04 23:25:30 +0530

    Your commit message here

--- db.js
+++ db.js
@@ -1,24 +1,18 @@
 const mongoose = require("mongoose");
 
-function connectDB(){
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb+srv://user2:123@cluster0.mmb99k4.mongodb.net/Sheycars', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
 
-    mongoose.connect('mongodb+srv://user2:123@cluster0.mmb99k4.mongodb.net/Sheycars' , {useUnifiedTopology: true , useNewUrlParser: true})
-
-    const connection = mongoose.connection
-
-    connection.on('connected' , ()=>{
-        console.log('Mongo DB Connection Successfull')
-    })
-
-    connection.on('error' , ()=>{
-        console.log('Mongo DB Connection Error')
-    })
-
-
+        console.log('Mongo DB Connection Successful');
+    } catch (error) {
+        console.error('Mongo DB Connection Error:', error.message);
+    }
 }
 
-connectDB()
+connectDB();
 
-module.exports = mongoose
-
-//mongodb+srv://sathya:sathyapr@cluster0.dkuc0.mongodb.net/sheycars-udemy
+module.exports = mongoose;
